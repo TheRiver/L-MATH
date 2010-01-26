18195366df9066565cfb0ee5bd2427209877e05f
Rudy Neeser <rudy@cs.uct.ac.za>
2010-01-26 16:52:16 +0200

    Have provided licensing information.

--- vector-operations.lisp
+++ vector-operations.lisp
@@ -1,5 +1,40 @@
 (in-package #:l-math)
 
+;;; L-MATH: a library for simple linear algebra.
+;;; Copyright (C) 2009-2010 Rudolph Neeser
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;
+;;; Additional permission under GNU GPL version 3 section 7
+;;; 
+;;; Linking this library statically or dynamically with other modules is
+;;; making a combined work based on this library. Thus, the terms and
+;;; conditions of the GNU General Public License cover the whole
+;;; combination.
+;;; 
+;;; As a special exception, the copyright holders of this library give you
+;;; permission to link this library with independent modules to produce an
+;;; executable, regardless of the license terms of these independent
+;;; modules, and to copy and distribute the resulting executable under
+;;; terms of your choice, provided that you also meet, for each linked
+;;; independent module, the terms and conditions of the license of that
+;;; module. An independent module is a module which is not derived from or
+;;; based on this library. If you modify this library, you may extend this
+;;; exception to your version of the library, but you are not obligated to
+;;; do so. If you do not wish to do so, delete this exception statement
+;;; from your version.
+
 ;;;---------------------------------------------------------------------
 ;;; Vector operations
 ;;;---------------------------------------------------------------------
@@ -10,13 +45,19 @@
     (test-dimensions lhs rhs)
     (with-slots ((lhs-data data)) lhs
       (with-slots ((rhs-data data)) rhs
-	(unless (= (cl:length lhs-data)
-		   (cl:length rhs-data))
-	  (error 'dimension-error :dim1 (cl:length lhs-data)
-		 :dim2 (cl:length rhs-data)))
 	(loop for l across lhs-data
 	   for r across rhs-data
-	   sum (* l r))))))
+	   sum (* l r)))))
+  (:method ((lhs list) (rhs vector))
+    (dot-product (to-vector lhs) rhs))
+  (:method ((lhs vector) (rhs list))
+    (dot-product lhs (to-vector rhs)))
+  (:method ((lhs list) (rhs list))
+    (test-dimensions lhs rhs)
+    (loop
+       for l in lhs
+       for r in rhs
+       sum (* l r))))
 
 (defgeneric angle-between (from to)
   (:documentation "Returns the angle between two vectors. The angle is
@@ -60,6 +101,14 @@
 	  for i across lhs-data
 	  for j across rhs-data
 	  sum (expt (- i j) 2))))))
+
+(defmethod euclidean-distance ((lhs list) (rhs list))
+  "Calculates the Euclidean distance between two vectors, represented
+as lists."
+  (loop
+     for l in lhs
+     for r in rhs
+     sum (expt (- l r) 2)))
 
 ;;;-------------------------------------------------------------------
 ;;; Operations on numbers
