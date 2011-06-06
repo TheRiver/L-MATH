be6f0d81a411141773dca88222f64d969047d7d7
Rudy Neeser <rudy.neeser@gmail.com>
2011-06-06 17:10:28 +0200

    Updated copyright date.

--- package.lisp
+++ package.lisp
@@ -1,5 +1,5 @@
 ;;; L-MATH: a library for simple linear algebra.
-;;; Copyright (C) 2009-2010 Rudolph Neeser
+;;; Copyright (C) 2009-2011 Rudolph Neeser
 ;;;
 ;;; This program is free software: you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
